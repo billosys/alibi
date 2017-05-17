576c997b86f46b726caef0b9f9b589937d29226a
Dechen Li <ryugi@billo.systems>
2017-05-16 22:46:29 -0500

    Fixed node build.

--- dev-resources/make/cljs.mk
+++ dev-resources/make/cljs.mk
@@ -3,16 +3,21 @@ NODE_MODS = ./node_modules
 NODE_MODS_PATH = $(JS_DIR)/$(NODE_MODS)
 CLJS_INSTALL_DIR = ./resources/public/dist
 
-build-cljs: $(NODE_MODS_PATH)
-	@lein cljsbuild once min
-
-build-cljs-dev: $(NODE_MODS_PATH)
+build-cljs: node
+	@# XXX min isn't working right now ...
+	@#@lein cljsbuild once min
 	@lein cljsbuild once
+
+build-cljs-dev: node
+	@lein cljsbuild once
+
+node: $(NODE_MODS_PATH)
+	@cd $(JS_DIR) && \
+	 $(NODE_MODS)/.bin/grunt copy
 
 $(NODE_MODS_PATH):
 	@cd $(JS_DIR) && \
-	 npm install && \
-	 $(NODE_MODS)/.bin/grunt copy
+	 npm install
 
 clean-cljs:
 	@rm -rf $(CLJS_INSTALL_DIR)
