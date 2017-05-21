2447bd247481f16d2a64e38e49ccd7e18095b99a
Dechen Li <ryugi@billo.systems>
2017-05-20 20:00:09 -0500

    Namespace reorg.

--- project.clj
+++ project.clj
@@ -9,8 +9,8 @@
     [bouncer "1.0.1" :exclusions [com.andrewmcveigh/cljs-time]]
     [buddy/buddy-auth "1.4.1"]
     [buddy/buddy-sign "1.5.0"]
-    [clj-http "2.1.0"]
-    [clj-time "0.13.0"]
+    [clj-http "2.1.0" :exclusions [commons-io]]
+    [clj-time "0.13.0" :exclusions [joda-time]]
     [clojure.jdbc/clojure.jdbc-c3p0 "0.3.2"]
     [clojusc/trifl "0.1.0-SNAPSHOT"]
     [clojusc/twig "0.3.2-SNAPSHOT"]
@@ -27,29 +27,30 @@
     [org.webjars/bootswatch-superhero "3.3.7"]
     [org.webjars/font-awesome "4.7.0"]
     [org.webjars/jquery "3.2.0"]
-    [org.xerial/sqlite-jdbc "3.16.1"]
+    [org.xerial/sqlite-jdbc "3.18.0"]
     [ring-logger "0.7.7"]
     [ring-webjars "0.2.0"]
     [ring.middleware.conditional "0.2.0"]
     [ring/ring-codec "1.0.1"]
     [ring/ring-core "1.6.1"]
-    [selmer "1.10.7"]]
+    [selmer "1.10.7" :exclusions [joda-time]]]
   :repl-options {
-    :init-ns user}
+    :init-ns timi.dev}
   :plugins [
     [cider/cider-nrepl "0.10.0"]
     [lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]
     [lein-figwheel "0.5.8"]
     [lein-pprint "1.1.1"]
     [lein-ring "0.9.7"]]
-  :main timi.core
+  :source-paths ["src/clj"]
+  :main timi.server.core
   :cljsbuild {
     :builds
       [{:id "dev"
         :source-paths ["src/cljs"]
         :figwheel {}
         :compiler {
-          :main timi.core
+          :main timi.client.core
           :asset-path "/dist/cljs/out"
           :output-to "resources/public/dist/cljs/timi.js"
           :output-dir "resources/public/dist/cljs/out"
@@ -60,7 +61,7 @@
         :source-paths ["src/cljs"]
         :compiler {
           :output-to "resources/public/dist/cljs/timi.js"
-          :main timi.core
+          :main timi.client.core
           :output-dir "resource/public/dist/cljs/out-single-file"
           :optimizations :simple
           :language-in :ecmascript5
@@ -69,7 +70,7 @@
         :source-paths ["src/cljs"]
         :compiler {
           :output-to "resources/public/dist/cljs/timi.js"
-          :main timi.core
+          :main timi.client.core
           :optimizations :advanced
           :externs [
             "externs/js-joda.js"
@@ -81,26 +82,22 @@
           :pretty-print false}}]}
   :figwheel {
     :server-port 5076
-    :css-dirs ["resources/public/res/css"]
+    :css-dirs ["resources/public/css"]
     :server-logfile "var/logs/figwheel.log"}
   :profiles {
     :uberjar {
-      :aot :all}
+      :aot [#"timi.server.*" #"timi.config.*"]}
     :dev [{
       :dependencies [
         [binaryage/devtools "0.9.4"]
-        [cljsjs/react "15.5.0-0"]
         [com.cemerick/piggieback "0.2.1"]
         [figwheel-sidecar "0.5.10"]
         [org.clojure/clojurescript "1.9.542"]
         [org.clojure/core.async "0.3.442"
          :exclusions [org.clojure/tools.reader]]
-        [org.omcljs/om "0.9.0"
-         :exclusions [cljsjs/react]]
+        [org.omcljs/om "0.9.0"]
         [ring/ring-mock "0.3.0"]]
       :source-paths [
-        "src/clj"
-        "src/cljs"
         "dev-resources/src"]
       :repl-options {
         :init (set! *print-length* 50)
@@ -122,8 +119,17 @@
            :custom-persistence]
     :custom-persistence {}}
   :aliases {
-    "timi-init" ["run" "sqlite" "create-db" :filename]
-    "timi-create-project" ["with-profile" "+local" "run" "projects" "create" :name]
-    "timi-create-task" ["with-profile" "+local" "run" "tasks" "create" :name]
+    "timi-help" [
+      "with-profile" "+local"
+      "run" "-m" "timi.cli"]
+    "timi-init" [
+      "with-profile" "+local"
+      "run" "-m" "timi.cli" "sqlite" "create-db" :filename]
+    "timi-create-project" [
+      "with-profile" "+local"
+      "run" "-m" "timi.cli" "projects" "create" :name]
+    "timi-create-task" [
+      "with-profile" "+local"
+      "run" "-m" "timi.cli" "tasks" "create" :name]
     "timi-run" ["with-profile" "+local" "run"]
     "check-deps" ["with-profile" "+test" "ancient" "check" "all"]})
