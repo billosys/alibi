478042d61cea70860e49249b04760e8b38ae5f59
Dechen Li <ryugi@billo.systems>
2017-05-23 23:57:35 -0500

    Created an initial basic CLI client to the CLI TCP server.

---
+++ src/cljs/cli/timi/client/cli.cljs
@@ -0,0 +1,46 @@
+(ns timi.client.cli
+  (:require
+    [cljs.nodejs :as node]
+    [clojure.string :as string]
+    [clojusc.twig :as logger]
+    [taoensso.timbre :as log]
+    [timi.client.config :as config]
+    [timi.client.tcp :as tcp]))
+
+;;; CLI setup and functions
+
+(node/enable-util-print!)
+
+(logger/set-level! (get-in config/data [:cli :client :log :ns])
+                   (get-in config/data [:cli :client :log :level]))
+
+(defn args->str
+  [args]
+  (str (string/join "\n" args) "\n"))
+
+;;; Callbacks
+
+(defn handle-connect
+  [client data]
+  (log/debug "Connected.")
+  (tcp/send client data))
+
+(defn handle-receive
+  [client data]
+  (let [buffer (js/Buffer. data)]
+    (log/debug "Received data:" data)
+    (tcp/disconnect client)
+    (log/debug "Disconnected.")
+    (println (str data))))
+
+;;; Main
+
+(defn -main
+  [& args]
+  (log/debug "Got args:" args)
+  (let [client (tcp/connect config/data)
+        data (args->str args)]
+    (tcp/on-connect client #(handle-connect client data))
+    (tcp/on-receive client #(handle-receive client %))))
+
+(set! *main-cli-fn* -main)
