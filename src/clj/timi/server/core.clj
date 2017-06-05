00115acb5334ba66c1f0f4764b824daba5e0db95
Dechen Li <ryugi@billo.systems>
2017-06-04 23:09:16 -0500

    Added docstrings.

--- src/clj/timi/server/core.clj
+++ src/clj/timi/server/core.clj
@@ -11,12 +11,17 @@
   (:gen-class))
 
 (defn get-system
+  "It is often useful for other parts of the application to be able to obtain a
+  system data structure (e.g., running a dev system or bringing up the
+  components needed for a CLI), so this function is provided as a universal
+  means of doing so."
   ([]
     (get-system #'web/app))
   ([app]
     (components/init app)))
 
 (defn -main
+  "The Tímı application's entry point."
   [& args]
   ;; Set up default logging here because the system hasn't had a chance to
   ;; it yet; once the configuration is available, the logger will re-configure.
